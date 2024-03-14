544d9e69648eb58b75397ddef300ef5a048ec5ae
Rafiq Sofi <106684888+rafiqbinsofi@users.noreply.github.com>
2024-03-14 13:47:05 +0800

    added some comment

--- sampinLine.js
+++ sampinLine.js
@@ -1,7 +1,9 @@
 export function imageLink() {
   let result = [];
   let limit;
+  // Change samping Line here
   for (let letter of ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K"]) {
+    // Change limit to represent the number of sampin in each line
     switch (letter) {
       case "A":
         limit = 10;
